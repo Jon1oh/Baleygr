3ee9349cab6143c7d016e59e3015ae54f82d2f84
lgf2111 <lgf2111@gmail.com>
2022-10-22 19:37:11 +0800

    tailwind not working

--- django_plus/settings.py
+++ django_plus/settings.py
@@ -31,7 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "experiment.apps.ExperimentConfig",
+    "home.apps.HomeConfig",
+    "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy-tailwind",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -122,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
