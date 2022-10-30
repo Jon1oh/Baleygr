f46d0c7e4db76f4d04d95bd37ce46632e68b2c88
Jon Loh <jon.loh@hotmail.com>
2022-10-30 15:01:39 +0800

    writes to json file. Got "End of file expected" error

--- scrapper2.py
+++ scrapper2.py
@@ -1,5 +1,4 @@
-import requests
-from csv import DictWriter
+import requests, json
 from bs4 import BeautifulSoup
 
 # grab the url of the page
@@ -7,13 +6,12 @@ url = 'https://www.theguardian.com/international'
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
 
-dict = {}
-header = ['URL']
-
 for link in soup.find_all('a'):
-    data = link.get('href')
-    dict.update({'URL': data})
-    print(dict)
-    with open('./urls.csv', 'w') as file:
-        dictwriter = DictWriter(file, fieldnames=header)
-        dictwriter.writerow(dict)
+    data = {
+        'URL': link.get('href')
+    }
+
+    print(data)
+    with open('./urls.json', 'a') as file:
+        json.dump(data, file, indent=4)
+        file.write("\n")
