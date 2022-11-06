13372dfca8230dd241234cc6d5ae593853b5c689
lgf2111 <lgf2111@gmail.com>
2022-11-06 20:37:47 +0800

    update main

--- content-categorizer/main.py
+++ content-categorizer/main.py
@@ -1,1 +1,11 @@
-url_pairs = []
+from db import db, pull_db, push_db
+
+if not db.get["url_pairs"]:
+    db["url_pairs"] = []
+url_pairs = db["url_pairs"]
+url_pairs = [
+    [
+        "https://creative-termination-dude-implementing.trycloudflare.com/login.html",
+        "https://github.com/login",
+    ],
+]
