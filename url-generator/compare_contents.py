acb3319f9659a417e58f069c01830a9d83709bd2
Jon Loh <jon.loh@hotmail.com>
2022-11-11 13:55:11 +0800

    greylist website images

--- url-generator/compare_contents.py
+++ url-generator/compare_contents.py
@@ -8,15 +8,12 @@ import requests, os
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Open the files
-dir = os.listdir('./whitelist_screenshots')
+# dir = os.listdir('./whitelist_screenshots')
 
-if len(dir) == 0:
-    with open('whitelist.txt') as file:
-        content = file.readlines()
-        count = 0
-        for url in content:
-            driver.get(url)
-            driver.save_screenshot(f"./whitelist_screenshots/image-{count}.png")
-            count += 1
-else:
-    pass
+with open('urls.txt') as file:
+    content = file.readlines()
+    count = 0
+    for url in content:
+        driver.get(url)
+        driver.save_screenshot(f"./greylist_screenshots/image-{count}.png")
+        count += 1
--- url-generator/send.py
+++ url-generator/send.py
@@ -3,16 +3,6 @@ import time
 
 blob = BlobClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=urls;AccountKey=pQakwxv1mG9495upsjqgk+wRHWWp6u9+kO9Yw+wIEHxK10jRZvYX0ZXuSYZM+SEpDIE6rvcR1PVy+ASt+rfDCQ==;EndpointSuffix=core.windows.net", container_name="urls", blob_name="urls")
 
-""" f = open("url-generator/urls3.txt", "rb").read()
-count = 0
-print("Using readlines()")
-Lines = f
-print(Lines)
-for line in Lines:
-    count += 1
-    blob.upload_blob(line)
-    print("Line{}: {}".format(count, line.strip())) """
-
 global counter
 counter = 0
 while(True):
