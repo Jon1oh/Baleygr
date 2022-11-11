e85008aa973592cba7a60765ea6391ead2681ac2
Jon Loh <jon.loh@hotmail.com>
2022-11-11 17:35:10 +0800

    update

--- generator/temp.py
+++
@@ -1,7 +0,0 @@
-from db import db, push_db, pull_db
-
-with open("url-generator/urls.txt", "r") as f:
-    urls = f.read().splitlines()
-for url in urls:
-    push_db("urls", url)
-    print("Pushed: " + url)
