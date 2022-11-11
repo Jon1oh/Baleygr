fa91d61c23b6803a2d1d0fa7710ba9f132885146
Jon Loh <jon.loh@hotmail.com>
2022-11-11 16:24:37 +0800

    automation testing

--- url-generator/receive.py
+++ url-generator/receive.py
@@ -1,21 +1,20 @@
 from azure.storage.blob import BlobClient
-import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
+# Install Driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 blob = BlobClient.from_connection_string(conn_str="DefaultEndpointsProtocol=https;AccountName=urls;AccountKey=pQakwxv1mG9495upsjqgk+wRHWWp6u9+kO9Yw+wIEHxK10jRZvYX0ZXuSYZM+SEpDIE6rvcR1PVy+ASt+rfDCQ==;EndpointSuffix=core.windows.net", container_name="urls", blob_name="urls")
-
-
 data = blob.download_blob()
-
 dataread = data.readall().decode()
-
 finale = dataread.split("\n")
 
-with open("urldownload.csv",  "w") as csv_file:
-    wr = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-    for word in finale:
-        wr.writerow([word])
-    
-
+count = 0
+for url in finale:
+    driver.get(url)
+    driver.save_screenshot(f"./testing/image-{count}.png")
+    count += 1
 
 
 
